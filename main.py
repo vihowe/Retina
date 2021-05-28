8e9a798085eb56b8079a0913a2083efb7a2b734d
vihowe <vihowe@qq.com>
2021-05-28 14:37:01 +0800

    serving

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from retina.option import Option
+from retina.serving import Serving
+from retina.option import Option, parse_options
 from retina.profiler import Profiler
 
 if __name__ == '__main__':
-    runtime_config = Option()
-    profiler = Profiler(runtime_config)
-    profiler.profile()
+    runtime_config = parse_options()
+
+    if runtime_config.task == "serve":
+        client = Serving(runtime_config)
+        start_time, end_time = client.querying()
+        
+    
--- retina/option.py
+++ retina/option.py
@@ -1,16 +1,21 @@
+from random import choices
 import torchvision.models as models
 import os
+import argparse
 
 
 class Option(object):
-    def __init__(
-            self,
-    ):
-        self.model_list = {
+    def __init__(self, args):
+        self.task = args.task
+        self.device = args.device
+
+        self.model_list = [
             # "resnet50": models.resnet50(),
             # "resnet152": models.resnet152(),
-            "resnet152": {'path':'../models/', 'total_segs': 52}
-        }
+            {
+                "resnet152": {'path':'/home/vihowe/courses/nn/Retina/models/', 'total_segs': 52}
+            }
+        ]
 
         self.supported_bs = [
             2,
@@ -27,3 +32,26 @@ class Option(object):
         self.cur_bs = 32
 
         self.test_loop = 100
+
+def parse_options():
+    parser = argparse.ArgumentParser(description="Retina")
+
+    parser.add_argument(
+        "--task",
+        type=str,
+        default="serve",
+        required=True,
+        choices=["profile", "serve"],
+    )
+
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda",
+        choices=["cpu", "cuda"]
+    )
+
+    args = parser.parse_args()
+
+    run_config = Option(args=args)
+    return run_config
